0bbf03d5227c7d926222f2dcebf5c0eefa58cf23
Marci <hajdumarcel@gmail.com>
2021-03-12 15:23:49 +0100

    finished the pageLoad module

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,8 +1,16 @@
 
 function createH1() {
+  const pageDiv = document.createElement('div');
   const h1Element = document.createElement('h1');
-  h1Element.innerHTML = 'Default Restaurant'
-  return h1Element;
+  const pElem = document.createElement('p');
+  h1Element.innerHTML = 'Default Restaurant';
+  let img = document.createElement('img');
+  img.src = "https://media-cdn.tripadvisor.com/media/photo-s/11/fa/54/9c/cbd.jpg";
+  pElem.innerHTML = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium aliquam magni eius magnam, ratione itaque modi omnis facilis aperiam nam iusto? Commodi ab, tenetur nobis assumenda tempore adipisci esse fuga.";
+  pageDiv.appendChild(h1Element);
+  pageDiv.appendChild(img);
+  pageDiv.appendChild(pElem);
+  return pageDiv;
 }
 
 export default createH1
