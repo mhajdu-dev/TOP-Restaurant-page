170c2c9c2a1dab530f55e51032fc3c0288397ef5
Marci <hajdumarcel@gmail.com>
2021-03-12 13:58:02 +0100

    made a feature, what is succesfully adding an H1 to the HTML using modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import createH1 from './pageLoad'
+
+let mainDiv = document.getElementById('content');
+console.log(mainDiv)
+console.log(createH1())
+mainDiv.appendChild(createH1())
+//mainDiv.innerHTML = createH1();
+
+
 console.log('webpack is working fine');
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -2,5 +2,7 @@
 function createH1() {
   const h1Element = document.createElement('h1');
   h1Element.innerHTML = 'Default Restaurant'
-
+  return h1Element;
 }
+
+export default createH1
